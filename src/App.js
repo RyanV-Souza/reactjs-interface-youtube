c09c198f6d597809b7400d7f03be6f3d0ea4d130
RyanV-Souza <ryanvsouza99@gmail.com>
2020-07-19 14:06:46 -0300

    Projeto Finalizado, troca de temas feita

--- src/App.js
+++ src/App.js
@@ -3,36 +3,38 @@ import { ThemeProvider, createMuiTheme, makeStyles } from '@material-ui/core';
 
 import Home from './Home';
 
-function App() {
+const useStyles = makeStyles({
+  root: {},
+});
 
+function App() {
   const [darkMode, setDarkMode] = useState(false);
 
   const theme = createMuiTheme({
+    spacing: 4,
     palette: {
-      type:  darkMode ? 'dark' : 'light',
-      spacing: 4,
+      type: darkMode ? 'dark' : 'light',
       primary: {
-        main: '#ff4336',
+        main: '#f44336',
       },
       secondary: {
         main: '#3EA6FF',
       },
       background: {
         default: darkMode ? '#232323' : '#FFF',
-        dark: darkMode ?'#181818' : '#F4F6F8',
+        dark: darkMode ? '#181818' : '#f4f6f8',
         paper: darkMode ? '#232323' : '#FFF',
-      }
-    }
+      },
+    },
   });
 
+  
 
   return (
     <ThemeProvider theme={theme}>
-      <Home darkMode={darkMode} setDarkMode={setDarkMode}/>
+      <Home darkMode={darkMode} setDarkMode={setDarkMode} />
     </ThemeProvider>
-  )
+  );
 }
 
-
 export default App;
-
